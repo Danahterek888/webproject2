9fd45d2695da4bba0aa114187d0fcdfcb808fe80
Danahterek888 <52310106@students.liu.edu.lb>
2025-12-31 19:50:38 +0200

    env and index.js updated

--- backend/index.js
+++ backend/index.js
@@ -18,11 +18,12 @@ app.listen(PORT, () => {
 
 // Database connection
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE
 });
+
 
 // Test DB connection
 db.connect((err) => {
