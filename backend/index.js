01d0e756e2c5e92cd5242849bedcf78b0e5e8282
Danahterek888 <52310106@students.liu.edu.lb>
2025-12-31 19:44:45 +0200

    backend port updated

--- backend/index.js
+++ backend/index.js
@@ -9,9 +9,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
+
 
 // Database connection
 const db = mysql.createConnection({
